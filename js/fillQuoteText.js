a5f5d58c83573222a8f8a3977c8c81d279e1db3b
Velenir <Velenir@users.noreply.github.com>
2016-06-28 23:11:24 +0300

    Connected #quote-text fill function to #getQuote button with states

--- js/fillQuoteText.js
+++ js/fillQuoteText.js
@@ -1,17 +1,20 @@
 const $output = $("#quote-text");
 const $getQuote = $("#get-quote");
 
+let quoteFilled = false, quoteTextBeingFilled = false, quoteBeingFilled = false, quoteTextInterval;
+const charInputFrequency = 35;
+
 function outputByChar(str, freq=100, cb, done) {
 	const len = str.length;
 	if(len === 0) return;
 	let ind = 0;
 
-	const intervalId = setInterval(produceChar, freq);
+	quoteTextInterval = setInterval(produceChar, freq);
 
 	function produceChar() {
 		cb(str[ind++]);
 		if(ind === len) {
-			clearInterval(intervalId);
+			clearInterval(quoteTextInterval);
 			if(done) done();
 		}
 	}
@@ -24,18 +27,40 @@ function addToOutput(s) {
 const inputStr = "I have come to believe that the whole world is an enigma, a harmless enigma that is made terrible by our own mad attempt to interpret it as though it had an underlying truth.";
 
 function fillQuoteText() {
+	quoteTextBeingFilled = quoteBeingFilled = true;
 	$output.text("");
+	$getQuote.addClass("on");
 	//const str = input.value;
 	// to be done in 7sec
 	// const freq = 5000/inputStr.length;
 	// console.log("freq", freq);
 
-	outputByChar(inputStr, 29, addToOutput, console.log.bind(console, "DONE"));
+	outputByChar(inputStr, charInputFrequency, addToOutput, onQuoteFilled);
+}
+
+function onQuoteTextFilled() {
+	quoteTextBeingFilled = false;
+}
+
+// call after #quote-text and #quote-author are finished
+function onQuoteFilled() {
+	quoteFilled = true;
+	quoteBeingFilled = false;
+	$getQuote.removeClass("on");
 }
 
 function fillQuoteTextImmediately() {
+	clearInterval(quoteTextInterval);
 	$output.text(inputStr);
+	// onQuoteTextFilled();
+	onQuoteFilled();
 }
 
 
-$getQuote.click(fillQuoteText);
+$getQuote.click(function () {
+	if(quoteBeingFilled) {
+		fillQuoteTextImmediately();
+	} else {
+		fillQuoteText();
+	}
+});
