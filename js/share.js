f5bd91aae4e38a5c81518e188983120c733394f2
Velenir <Velenir@users.noreply.github.com>
2016-07-01 15:18:04 +0300

    Added share to twitter functionality

---
+++ js/share.js
@@ -0,0 +1,23 @@
+//////////////////////////////////////////////////////
+// SHARE LOGIC
+
+	document.getElementById("twit").onclick = function (e) {
+		e.preventDefault();
+		e.stopPropagation();
+
+		if(!quote) return;
+
+		const width  = 575,
+			height = 450,
+			left   = (window.innerWidth - width)/2,
+			top    = (window.innerHeight - height)/2,
+			url    = `https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${encodeURIComponent(`"${quote.quote}" ${quote.author}`)}`,
+			opts   = `status=1,
+								resizable=1,
+								width=${width},
+								height=${height},
+								left=${left},
+								top=${top}`;
+
+		window.open(url, 'twitter', opts);
+	};
